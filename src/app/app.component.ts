9692291524aed7edfe2621afc732b4993967657d
iamjesse98 <yadagallajaswanthkumar98@gmail.com>
2018-06-06 15:28:50 +0530

    ss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,15 +11,16 @@ export class AppComponent {
   myData: Array<any>;
   displayedColumns: Array<any>;
   myDate: any;
+  myType: any;
 
   constructor(private http: HttpClient) {
 
   }
   getData() {
     const dte = new Date(this.myDate);
-    console.log(dte.getDate(), dte.getFullYear());
+    // console.log(dte.getDate(), dte.getFullYear());
     const months = [ 'JANUARY', 'FEBRUARY', 'MARCH', 'APRIL', 'MAY', 'JUNE', 'JULY', 'AUGUST', 'SEPTEMBER', 'NOVEMBER', 'DECEMBER' ];
-    this.http.get(`http://localhost:3003/get/${dte.getFullYear()}/${months[dte.getMonth()]}/${dte.getDate()}/bhav`)
+    this.http.get(`http://localhost:3003/get/${dte.getFullYear()}/${months[dte.getMonth()]}/${dte.getDate()}/${this.myType}`)
       .subscribe(res => {
         const { fields, row } = res
         this.displayedColumns = fields;
