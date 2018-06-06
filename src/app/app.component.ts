4e9a48bd6a64ce189de7a8a2e61ce2604cd0d201
iamjesse98 <yadagallajaswanthkumar98@gmail.com>
2018-06-06 14:12:55 +0530

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  myData: Array<any>;
+  displayedColumns: Array<any>;
+
+  constructor(private http: HttpClient) {
+
+  }
+  getData() {
+    console.log('hello');
+    this.http.get(`http://localhost:3003/get/2017/MAY/29/bhav`)
+      .subscribe(res => {
+        this.displayedColumns = res.fields;
+        this.myData = res.row
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { MatButtonModule, MatTableModule, } from '@angular/material';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +11,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatTableModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
